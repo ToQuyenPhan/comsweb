e12848e30c115bc64a248a6f614fc013e92784ca
ToQuyenPhan <duonglh024@gmail.com>
2023-09-28 07:46:01 +0700

    add login page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import LoginPage from './pages/UserLoginPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<LoginPage/>}/>
+      <Route exact path="/login" element={<LoginPage/>}/>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/UserLoginPage/index.jsx
@@ -0,0 +1,59 @@
+import { Link } from 'react-router-dom';
+import { FaUserShield } from 'react-icons/fa';
+import { BsFillShieldLockFill } from 'react-icons/bs';
+import { AiOutlineSwapRight } from 'react-icons/ai';
+import './css/style.css';
+import contractManagementImg from '../../assets/contractmanagementlogo.png';
+import logoImg from '../../assets/hisoftlogo.png';
+
+function UserLogin() {
+    return(
+        <div className="loginPage flex">
+            <div className="container flex">
+                <div className="imgDiv">
+                    <img className="contractImg" src={contractManagementImg} alt='/' />
+                    <div className="overlayDiv"></div>
+                    <div className="textDiv">
+                        <h2 className="title">
+                            Hisoft Contract Management System
+                        </h2>
+                    </div>
+                    <div className="footerDiv">
+                        <span className='text'>If you are our partner, please enter review code!</span>
+                        <Link>
+                            <button className='btn'>Enter Code</button>
+                        </Link>
+                    </div>
+                </div>
+                <div className="formDiv">
+                    <div className="headerDiv">
+                        <img className='logo' src={logoImg} alt='Hisoft Logo' />
+                        <h3 className='welcome'>Welcome Back!</h3>
+                    </div>
+                    <form action='' className='form grid'>
+                        <div className="inputDiv">
+                            <label className="label" htmlFor='username'>Username</label>
+                            <div className="input flex">
+                                <FaUserShield className='icon' />
+                                <input className="inputData" type='text' id='username' placeholder='Enter Username' required />
+                            </div>
+                        </div>
+                        <div className="inputDiv">
+                            <label className="label" htmlFor='password'>Password</label>
+                            <div className="input flex">
+                                <BsFillShieldLockFill className='icon' />
+                                <input className="inputData" type='password' id='password' placeholder='Enter Password' required />
+                            </div>
+                        </div>
+                        <button className="btn">
+                            <span>Login</span>
+                            <AiOutlineSwapRight className='icon' />
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UserLogin;
