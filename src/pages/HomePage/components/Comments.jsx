316331aff757393935c6241d11445bb80c960ceb
ToQuyenPhan <duonglh024@gmail.com>
2023-11-21 23:11:45 +0700

    add View Comments

--- src/pages/HomePage/components/Comments.jsx
+++ src/pages/HomePage/components/Comments.jsx
@@ -1,48 +1,125 @@
 import React, { useEffect, useState } from "react";
 import { Icon } from '@iconify/react';
-import { $ } from 'react-jquery-plugin';
+import Swal from 'sweetalert2';
 import "../css/_comments.css";
-import { tns } from "../index";
 
 function Comments() {
-    const [autoPlay, setAutoPlay] = useState(true);
-    const [currentIndex, setCurrentIndex] = useState(0);
-    let timeOut = null;
+    const [comments, setComments] = useState([]);
+    // const [autoPlay, setAutoPlay] = useState(true);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [hasNext, setHasNext] = useState(false);
+    const [hasPrevious, setHasPrevious] = useState(false);
+    // let timeOut = null;
+    const token = localStorage.getItem("Token");
 
-    const prevComment = () => {
-        const isFirstComment = currentIndex === 0;
-        const newIndex = isFirstComment ? comments.length - 1 : currentIndex - 1;
-        setCurrentIndex(newIndex);
-    }
-
-    const nextComment = () => {
-        const isLastComment = currentIndex === comments.length - 1;
-        const newIndex = isLastComment ? 0 : currentIndex + 1;
-        setCurrentIndex(newIndex);
-    }
-
-    const comments = [
-        {
-            contractName: "Lorem Ipsum is simply dummy text",
-            createAt: "20 Hours ago",
-            content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
-        },
-        {
-            contractName: "abc",
-            createAt: "20 Hours ago",
-            content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
-        },
-        {
-            contractName: "Lorem Ipsum is simply dummy text",
-            createAt: "20 Hours ago",
-            content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+    const fetchCommentData = async () => {
+        let url = `https://localhost:7073/Comments/all?CurrentPage=1&PageSize=1`;
+        const res = await fetch(url, {
+            mode: 'cors',
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+        });
+        if (res.status === 200) {
+            const data = await res.json();
+            setComments(data.items);
+            setHasNext(data.has_next);
+            setHasPrevious(data.has_previous);
+            setCurrentPage(data.current_page);
+        } else {
+            const data = await res.json();
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: data.title
+            })
         }
-    ];
+    }
+
+    const fetchNext = async () => {
+        if (!hasNext) {
+            return;
+        }
+        const res = await fetch(`https://localhost:7073/Comments/all?CurrentPage=${currentPage + 1}&pageSize=1`, {
+            mode: 'cors',
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+        });
+        if (res.status === 200) {
+            const data = await res.json();
+            setComments(data.items);
+            setHasNext(data.has_next);
+            setHasPrevious(data.has_previous);
+            setCurrentPage(data.current_page);
+        } else {
+            const data = await res.json();
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: data.title
+            })
+        }
+    }
+
+    const fetchPrevious = async () => {
+        if (!hasPrevious) {
+            return;
+        }
+        const res = await fetch(`https://localhost:7073/Comments/all?CurrentPage=${currentPage - 1}&pageSize=1`, {
+            mode: 'cors',
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+        });
+        if (res.status === 200) {
+            const data = await res.json();
+            setComments(data.items);
+            setHasNext(data.has_next);
+            setHasPrevious(data.has_previous);
+            setCurrentPage(data.current_page);
+        } else {
+            const data = await res.json();
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: data.title
+            })
+        }
+    }
+
+    const fetchDismissComment = async (id) => {
+        const res = await fetch(`https://localhost:7073/Comments/dismiss?id=${id}`, {
+            mode: 'cors',
+            method: 'PUT',
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+        });
+        if (res.status === 200) {
+            fetchCommentData();
+        } else {
+            const data = await res.json();
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: data.title
+            })
+        }
+    }
 
     useEffect(() => {
-        timeOut = autoPlay && setTimeout(() => {
-            nextComment();
-        }, 2500)
+        fetchCommentData();
+        // timeOut = autoPlay && setTimeout(() => {
+        //     fetchNext();
+        // }, 2500)
     }, []);
 
     return (
@@ -51,22 +128,29 @@ function Comments() {
                 <h2>
                     Important Notes
                 </h2>
-                <button onClick={prevComment} data-carousel="important-notes" data-target="prev" className="tiny-slider-navigator btn dark:text-slate-300"> <Icon icon="lucide:chevron-left" className="icon" /> </button>
-                <button onClick={nextComment} data-carousel="important-notes" data-target="next" className="tiny-slider-navigator btn dark:text-slate-300"> <Icon icon="lucide:chevron-right" className="icon" /> </button>
+                <button onClick={fetchPrevious} disabled={!hasPrevious} data-carousel="important-notes" data-target="prev" 
+                    className={"tiny-slider-navigator btn dark:text-slate-300 " + (hasPrevious ? "" : "disabled")}> 
+                    <Icon icon="lucide:chevron-left" className="icon" /> </button>
+                <button onClick={fetchNext} disabled={!hasNext} data-carousel="important-notes" data-target="next" 
+                    className={"tiny-slider-navigator btn dark:text-slate-300 " + (hasNext ? "" : "disabled")}> <Icon icon="lucide:chevron-right" className="icon" /> 
+                    </button>
             </div>
             <div className="intro-x">
-                <div className="box zoom-in">
-                    <div className="tiny-slider" id="important-notes">
-                        <div onMouseEnter={() => {setAutoPlay(false)}} onMouseLeave={() => {setAutoPlay(true)}}>
-                            <div>{comments[currentIndex].contractName}</div>
-                            <div>{comments[currentIndex].createAt}</div>
-                            <div>{comments[currentIndex].content}</div>
+                {comments.map(comment => (
+                    <div id={comment.id} className="box zoom-in">
+                        <div className="tiny-slider" id="important-notes">
+                            {/* onMouseEnter={() => { setAutoPlay(false) }} onMouseLeave={() => { setAutoPlay(true) }} */}
                             <div>
-                                <button type="button" className="btn btn-secondary">View Notes</button>
-                                <button type="button" className="btn btn-outline-secondary">Dismiss</button>
+                                <div>{comment.fullName}</div>
+                                <div>{comment.long}</div>
+                                <div>{comment.content}</div>
+                                <div>
+                                    <button type="button" className="btn btn-secondary">View Notes</button>
+                                    <button type="button" className="btn btn-outline-secondary" 
+                                        onClick={() => fetchDismissComment(comment.id)}>Dismiss</button>
+                                </div>
                             </div>
-                        </div>
-                        {/* <div>
+                            {/* <div>
                             <div>Lorem Ipsum is simply dummy text</div>
                             <div>20 Hours ago</div>
                             <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</div>
@@ -93,8 +177,9 @@ function Comments() {
                                 <button type="button" className="btn btn-outline-secondary">Dismiss</button>
                             </div>
                         </div> */}
+                        </div>
                     </div>
-                </div>
+                ))}
             </div>
         </div>
     );
