fc40634dcce83144cf5924f28ee32a998e4c5ae0
ThanhHT <hotanthanhptg1@gmail.com>
2023-11-24 22:40:30 +0700

    Contract-Details-1.0: View contract with pdf

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import PartnerCodePage from './pages/PartnerCodePage';
 import HomePage from './pages/HomePage';
 import CreateTemplate from './pages/CreateTemplatePage';
 import TemplateList from './pages/TemplateListPage';
+import ContractDetails from './pages/ContractDetailsPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -18,6 +19,7 @@ root.render(
       <Route exact path="/home" element={<HomePage/>}/>
       <Route exact path="/template" element={<TemplateList />}/>
       <Route exact path="/create-template" element={<CreateTemplate/>}/>
+      <Route exact path="/contract-details" element={<ContractDetails/>}/>
     </Routes>
   </BrowserRouter>
 );
